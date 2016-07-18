5e75dd680af4c50557eeb573a88ea2a6eacb50ad
Francisco Vale <xico.vale@hotmail.com>
2016-07-18 20:45:17 +0100

    Fixed missing declaration

--- ceaser.hpp
+++ ceaser.hpp
@@ -4,6 +4,7 @@ class Caeser {
   public:
     std::string cypher(std::string text, int shift_value);
     std::string decypher(std::string text, int shift_value);
+    std::string crack(std::string text);
     Caeser(void);
 
   private:
