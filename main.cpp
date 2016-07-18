895018f5695d7a6b0cd4644b925ec687d56c496c
xicoVale <xico.vale@hotmail.com>
2016-07-18 18:11:03 +0100

    Program will now take command line arguments

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void help() {
   return ;
 }
 
-int main () {
+void interactive() {
   while (true) {
     string text;
     string result;
@@ -40,6 +40,45 @@ int main () {
       result = caeser.decypher(text, value);
     }
     cout << result << endl;
+  }
+  return ;
+}
+
+int main (int argc, char *argv[]) {
+  if (argc == 1) {
+    interactive();
+    exit(0);
+  }
+  else if (argc == 2) {
+    string arg(argv[1]);
+    if (arg == "--help" || arg == "-h") {
+      help();
+      exit(0);
+    }
+    else {
+      cerr << "Missing argument" << endl;
+      exit(1);
+    }
+  }
+  else if (argc == 3) {
+    string text(argv[1]);
+    int value = atoi(argv[2]);
+    Caeser caeser;
+
+    if (value < 0) {
+      string result = caeser.decypher(text, value);
+      cout << result << endl;
+      exit (0);
+    }
+    else if (value > 0) {
+      string result = caeser.cypher(text, value);
+      cout << result << endl;
+      exit (0);
+    }
+  }
+  else if(argc > 3) {
+    cerr << "Too many arguments." << endl;
+    exit (1);
   }
 
   return 0;
